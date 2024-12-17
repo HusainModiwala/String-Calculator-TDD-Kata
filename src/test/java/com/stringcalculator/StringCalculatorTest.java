78ff1a3ae2d2cec4f76d3bf4b5b3d4dceba883ef
HusainModiwala <hussainmodiwala@gmail.com>
2024-12-17 12:31:47 +0530

    added test case to check n number addition

--- src/test/java/com/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/stringcalculator/StringCalculatorTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class StringCalculatorTest {
+    // empty string test
     @Test
     public void addEmptyStringTest() {
         int actual = StringCalculator.add("");
@@ -11,6 +12,7 @@ public class StringCalculatorTest {
         Assert.assertEquals(expected, actual);
     }
 
+    // string with single no test
     @Test
     public void addOneNumberTest() {
         int actual = StringCalculator.add("1");
@@ -18,10 +20,19 @@ public class StringCalculatorTest {
         Assert.assertEquals(expected, actual);
     }
 
+    // string with two no test
     @Test
     public void addTwoNumbersTest() {
         int actual = StringCalculator.add("2,3");
         int expected = 5;
+        Assert.assertEquals(expected, actual);
+    }
+
+    // string with n no test
+    @Test
+    public void addNNumbersTest() {
+        int actual = StringCalculator.add("2,3,5,3,2,6,8,9,5");
+        int expected = 43;
         Assert.assertEquals(expected, actual);
     }
 }
